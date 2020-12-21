d942a0fcda97416809cf11b8f81ecf28b20706cf
kj44389 <47088522+kj44389@users.noreply.github.com>
2020-12-21 15:35:19 +0100

    Including foother from php file in all files

--- AddLicenses.php
+++ AddLicenses.php
@@ -141,14 +141,7 @@
           </div>
           <!-- End of Main Content -->
 
-      <footer class="sticky-footer bg-white">
-          <div class="container my-auto">
-              <div class="copyright text-center my-auto">
-                  <span>Copyright &copy; M&M's Karczma</span></br></br>
-                  <span>Authors: Kudzia, Czarnota, Olejarczyk, Marcinkowski</span>
-              </div>
-          </div>
-      </footer>
+      <?php require_once("foother.php") ?>
             </div>
       <!-- End of Content Wrapper -->
     </div>
